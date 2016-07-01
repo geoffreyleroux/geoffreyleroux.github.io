810620b8f02a5738778b4e307e85e3e33d9f948d
Geoffrey Leroux <geoffrey.leroux@in-tact.fr>
2016-07-01 17:20:02 +0200

    comment page

---
+++ src/states/comments/comments.js
@@ -0,0 +1,24 @@
+'use strict';
+
+angular.module('app')
+    .controller('CommentsCtrl', function($scope, $http, news, comments, $state) {
+        $scope.comment = "";
+        var refresh = function() {
+            $scope.loadingComments = true;
+            news.getOneNews($state.params.id).then(function(data) {
+                $scope.news = data;
+                $scope.news.createDate = moment($scope.news.createDate).fromNow();
+                $scope.loadingComments = false;
+            });
+        };
+        $scope.addComment = function() {
+            var comment = {
+                author: "geoffrey leroux",
+                content: $scope.comment
+            };
+            comments.addComments($scope.news._id, comment).then(function() {
+                refresh();
+            });
+        }
+        refresh();
+    });
