e5fa85ed3113195caa5794d18b51b25591d80119
Geoffrey Leroux <geoffrey.leroux@in-tact.fr>
2016-07-01 18:19:02 +0200

    refacto

--- src/states/news/submit.js
+++ src/states/news/submit.js
@@ -1,12 +1,16 @@
 'use strict';
 
 angular.module('app')
-	.controller('SubmitCtrl', function($scope, $http, news) {
-		$scope.news = {
-			vote: 0,
-			comments: []
-		};
-		$scope.addNews = function() {
-			news.addNews($scope.news);
-		};
-	});
+    .controller('SubmitCtrl', function($scope, $http, news, dialog, $state) {
+        $scope.news = {
+            vote: 0,
+            comments: []
+        };
+        $scope.addNews = function() {
+            news.addNews($scope.news).then(function() {
+                dialog.confirm("News successfully added.");
+                $state.go('webclient.home');
+            });
+        };
+
+    });
